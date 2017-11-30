e2833e0242c5c4aba1bfbab1ad403019adefd4d4
Gerhard Alfanz <gerhard.alfanz@magora.com>
2017-11-30 14:30:09 +0100

    Fixes #31: Wrong parameters error when TimeoutException is thrown (#32)
    * Fixes #31: Wrong parameters error when TimeoutException is thrown

--- src/Exception/StreamException.php
+++ src/Exception/StreamException.php
@@ -3,7 +3,36 @@
 namespace Http\Client\Socket\Exception;
 
 use Http\Client\Exception;
+use Psr\Http\Message\RequestInterface;
 
 class StreamException extends \RuntimeException implements Exception
 {
+    /**
+     * The request object.
+     *
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * Accepts an optional request object as 4th param.
+     *
+     * @param string           $message
+     * @param int              $code
+     * @param Exception        $previous
+     * @param RequestInterface $request
+     */
+    public function __construct($message = null, $code = null, $previous = null, RequestInterface $request = null)
+    {
+        $this->request = $request;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return \Psr\Http\Message\RequestInterface|null
+     */
+    final public function getRequest()
+    {
+        return $this->request;
+    }
 }
